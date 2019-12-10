0c892ebf1923adf5ea6a5fdfe222079c14954cc4
huancnttuet <huancnttuet@gmail.com>
2019-12-10 12:42:50 +0700

    first commit

---
+++ precache-manifest.2e07f5be6056c31f089510d585af589e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fcb5d5186f2e404fa4455e36ef65420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cd9912aeb68ea4f5ad0",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "4cd9912aeb68ea4f5ad0",
+    "url": "/static/js/2.5294c1c9.chunk.js"
+  },
+  {
+    "revision": "da0f22a77795218b8468",
+    "url": "/static/js/main.0552a342.chunk.js"
+  },
+  {
+    "revision": "b8cdcca1553dd9f9a271",
+    "url": "/static/js/runtime-main.2dc6df11.js"
+  },
+  {
+    "revision": "8cc15aae15c0316096eb281f558f4e1b",
+    "url": "/static/media/giphy.8cc15aae.gif"
+  }
+]);
